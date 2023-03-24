dcc8d70bf81da9c0f2667744efcbe3b079c5d71b
Abdurrahmanalghofiqi <abdurrahmanalghofiqi69@gmail.com>
2023-03-24 10:49:06 +0700

    mengenal procedure itnsertionsor

--- belajar insertion sort.cpp/belajar insertion sort.cpp.cpp
+++ belajar insertion sort.cpp/belajar insertion sort.cpp.cpp
@@ -7,8 +7,8 @@ int n;		  //Membuat Variabel inputan n
 void input() {	//Procedure input
 	while (true)
 	{
-		cout << "Masukan Jumlah Data pada Array"	//Membuat input jumlah elemen Array
-			cin >> n;								//memanggil variabel input n
+		cout << "Masukan Jumlah Data pada Array : "	//Membuat input jumlah elemen Array
+		cin >> n;								//memanggil variabel input n
 
 		if (n <= 20) {				//membuat kondisi n tidak lebih dari 20
 			break;
@@ -18,4 +18,7 @@ void input() {	//Procedure input
 			cout << "\nArray yang ada masukan maksimal 20 Elemen.\n";	//menampilkan pesan jika data lebih dari 20
 		}
 	}
-	cout
+	cout << endl;		//membuat jarak perbaris program
+	cout << "====================" << endl;			//membuat tampilan susunan data elemen array
+	cout << "Masukan Elemen Array" << endl;
+	cout << "=====================" << endl;
