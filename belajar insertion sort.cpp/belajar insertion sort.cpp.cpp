46abfeeb8096f56f2f91eccddb0073dc2108ccb2
Abdurrahmanalghofiqi <abdurrahmanalghofiqi69@gmail.com>
2023-03-24 10:58:55 +0700

    prosedur membuat variabel data temporer

--- belajar insertion sort.cpp/belajar insertion sort.cpp.cpp
+++ belajar insertion sort.cpp/belajar insertion sort.cpp.cpp
@@ -8,7 +8,7 @@ void input() {	//Procedure input
 	while (true)
 	{
 		cout << "Masukan Jumlah Data pada Array : "	//Membuat input jumlah elemen Array
-		cin >> n;								//memanggil variabel input n
+			cin >> n;								//memanggil variabel input n
 
 		if (n <= 20) {				//membuat kondisi n tidak lebih dari 20
 			break;
@@ -22,3 +22,39 @@ void input() {	//Procedure input
 	cout << "====================" << endl;			//membuat tampilan susunan data elemen array
 	cout << "Masukan Elemen Array" << endl;
 	cout << "=====================" << endl;
+
+
+	for (int i = 0; i < n; i++)	//menggunakan perulangan for untuk menyipan data pada array
+	{
+		cout << "Data ke-" << (i + 1) << "; ";	//memasukkan atau menginputkan nilai data n
+		cin >> arr[i];							//menyimpan nilai data n
+	}
+}
+
+void itnsertionsor() {		//procedure itnsertionsor
+
+	int temp;			//membuat variabel data temporer atau penyimpanan sementara
+	int j;				//membuat variabel j sebagai penanda
+
+	for (int i = 1; i < n; i++)		//1. looping dengan i dimulai dari 1 hingga n-1
+
+		temp = arr[i];				//2. simpan nilai arr[i] ke variabel sementara temp
+
+	j = i - 1;						//3. setting nilai j sama dengan i-1;
+
+	while (j >= 0 && arr[j] > temp)		//4. looping while dimana nilai j lebih besar sama dengan 0 dan 
+		//arr[j] lebih besar dari pada temp
+	{
+		arr[j + 1] = temp;	//4. simpan arr[j] ke dalam variabel arr[j + 1]
+		j--;				//4. Descrement nilai j by 1
+	}
+
+	arr[j + 1] = temp;		//5. simpan nilai temp ke dalam arr[j+1
+
+
+	cout << "\nPass " << i << "; ";		//Output ke layar 
+	for (int k = 0; k < n; k++) {		//looping nilai k dimulai dari 0 hingga n-1
+		cout << arr[k] << "";			//Output ke layar
+	}
+}
+}
