de2bd488c733a541cef23981190f125187fa5967
Abdurrahmanalghofiqi <abdurrahmanalghofiqi69@gmail.com>
2023-03-24 10:24:26 +0700

    memasukkan jumlah data

--- belajar insertion sort.cpp/belajar insertion sort.cpp.cpp
+++ belajar insertion sort.cpp/belajar insertion sort.cpp.cpp
@@ -4,6 +4,18 @@ using namespace
 int arr[20];  //Membuat Array dengan panjang data 20
 int n;		  //Membuat Variabel inputan n
 
-void input() {
-	white
-}
+void input() {	//Procedure input
+	while (true)
+	{
+		cout << "Masukan Jumlah Data pada Array"	//Membuat input jumlah elemen Array
+			cin >> n;								//memanggil variabel input n
+
+		if (n <= 20) {				//membuat kondisi n tidak lebih dari 20
+			break;
+		}
+		else
+		{
+			cout << "\nArray yang ada masukan maksimal 20 Elemen.\n";	//menampilkan pesan jika data lebih dari 20
+		}
+	}
+	cout
